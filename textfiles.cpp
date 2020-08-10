22b814e55b2193c8f14f1ac9fbe4a8b4149b4d25
Mayra Alejandra <mayra.vargasc@upb.edu.co>
2020-08-10 17:38:53 -0500

    i

--- textfiles.cpp
+++ textfiles.cpp
@@ -12,15 +12,15 @@ using namespace std;
 
 void parseCSV()
 {
-    std::ifstream  data("Book1.csv");
-    std::string line;
-    std::vector<std::vector<std::string> > parsedCsv;
-    while(std::getline(data,line))
+    std::ifstream  data("Book1.csv"); //Se construye el objeto "data" para importar el archivo .csv
+    std::string line;   //Se define una string                 
+    std::vector<std::vector<std::string> > parsedCsv; //Se define un vector y dentro se define otro fector que contiene un string
+    while(std::getline(data,line))          // ciclo while para extraer elementos de el objeto "data" y guardarlos en el string "line"
     {
-        std::stringstream lineStream(line);
-        std::string cell;
-        std::vector<std::string> parsedRow;
-        while(std::getline(lineStream,cell,','))
+        std::stringstream lineStream(line); // Se define una clase para operar dentro del contenido del string "line"
+        std::string cell;   //Se define un string "cell" 
+        std::vector<std::string> parsedRow; //Se define un vector que contiene un string 
+        while(std::getline(lineStream,cell,',')) //Se extrae el contenido de la clase "lineStream", se guarda en "cell" y se usa un delimitante para separar los elementos
         {
             parsedRow.push_back(cell); // por medio de push back se agregan los valores dentro del vector, el push back va a agregando un valor al final del vector//
 
@@ -29,20 +29,19 @@ void parseCSV()
         parsedCsv.push_back(parsedRow);
 
 	}
-	 // inicialmente lo hice de forma manual esto era introducir las variables una por una quiere decir sin extraer del archivo.exe//
-     // luego lo pense de forma dinamica, esto se puede hacer recorriendo  los vectores directamente desde el archivo.exe//
+	 
     for (int i=0; i<parsedCsv.size(); i++){   // En este primer for lo que hacemos es recorrer el tamaño del primer vector, este primer vector son  las columnas//
         for (int j=0; j<parsedCsv[i].size(); j++){// En el segundo for lo que hacemos es recorrre el tamaño del segundo vector, este segundo vector  son las filas//
             cout << parsedCsv[i][j] << "   "; // finalmente hago la iteracion entre j Y i, aqui se imprimen tanto filas como columnas//
         }
-        cout << '\n';
+        cout << '\n';       //Se agrega un espaciado a los valores anteriores 
     }
 }
 
 
 int main()
 {
-	parseCSV();
+	parseCSV(); //se llama la funcion
 
 	return 0;
 }
