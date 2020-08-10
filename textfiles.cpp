7efd76e0910dfa98ce3c1b993962cd7dbbc3c437
Mayra Alejandra <mayra.vargasc@upb.edu.co>
2020-08-10 11:23:47 -0500

    lab_csv

---
+++ textfiles.cpp
@@ -0,0 +1,43 @@
+// Programa que lee un archivo CSV para texto
+// Creado por Cesar Aceros
+// Inspirado en: https://www.codespeedy.com/read-data-from-csv-file-in-cpp/
+//
+
+#include<fstream>
+#include <string>
+#include <vector>
+#include <sstream> 
+#include <iostream>
+using namespace std;
+
+void parseCSV()
+{
+    std::ifstream  data("Book1.csv");
+    std::string line;
+    std::vector<std::vector<std::string> > parsedCsv;
+    while(std::getline(data,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<std::string> parsedRow;
+        while(std::getline(lineStream,cell,','))
+        {
+            parsedRow.push_back(cell);
+        }
+
+        parsedCsv.push_back(parsedRow);
+
+	}
+	for (char archivo:parsedCsv){	
+		cout<<archivo;
+	}
+}
+
+
+int main()
+{
+	parseCSV();
+
+	return 0;
+}
+
